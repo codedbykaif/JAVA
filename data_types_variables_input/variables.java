8e71e46e4a6f4bb3a19de147364442488c009fc6
codedbykaif <kaif_246048@saitm.ac.in>
2025-08-21 14:22:19 +0530

    Add files via upload

---
+++ data_types_variables_input/variables.java
@@ -0,0 +1,12 @@
+public class variables {
+    public static void main(String[] args) {
+        int myNumber = 583;
+        int yourNumber;
+        yourNumber = 5353;
+        System.out.println(yourNumber);
+        yourNumber = 45;
+        System.out.println(yourNumber);
+
+
+    }
+}
