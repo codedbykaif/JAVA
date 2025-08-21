3cee06669f2d7a15ece715f5709b201ec1e61455
codedbykaif <kaif_246048@saitm.ac.in>
2025-08-21 15:33:17 +0530

    Add files via upload

--- data_types_variables_input/variables.java
+++ data_types_variables_input/variables.java
@@ -7,6 +7,18 @@ public class variables {
         yourNumber = 45;
         System.out.println(yourNumber);
 
+        float myFloat = 54;
+        double myDouble = 45.45;
+        System.out.println(myFloat);
+
+        boolean isVegeterian = true;
+        System.out.println(isVegeterian);
+
+        String wishes = "Kaif is dreamer";
+        System.out.println(wishes);
+
+        char myCharacter = 'K';
+        System.out.println(myCharacter);
 
     }
 }
