0074835247604e9cdd686ce1dff1f50df3bfa67b
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-22 12:06:18 +0530

    typecasting

--- src/typeconversion.java
+++ src/typeconversion.java
@@ -2,5 +2,8 @@ public class typeconversion {
     public static void main(String[] args){
         float myFloat = 4;
         System.out.println(myFloat);
+
+        int myInt = (int) 64.64f;
+        System.out.println(myInt);
     }
 }
