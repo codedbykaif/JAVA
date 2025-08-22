a11df91aa53f51fe9824357bbc08ad3e740334f7
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-22 11:41:47 +0530

    challenges

---
+++ src/data_types_variables_input/challenge.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class challenge{
+    public static void main(String[] args){
+        System.out.println("Enter your name: ");
+        Scanner input = new Scanner(System.in);
+         String name = input.nextLine();
+        System.out.println("Welcome " + name + " in dreamer's journey ");
+    }
+}
