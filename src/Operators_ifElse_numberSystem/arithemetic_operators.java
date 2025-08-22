aafbee0fe86c03f431edef86c20be60f7c4aba77
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-22 14:42:01 +0530

    Arithemetic

---
+++ src/Operators_ifElse_numberSystem/arithemetic_operators.java
@@ -0,0 +1,25 @@
+public class arithemetic_operators {
+    public static void main(String [] args) {
+//     integers arithemetic operations
+        int a = 543;
+        int b = 64;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println("\n\n");
+
+//        double arithemetic operations
+        double c = 534;
+        double d = 2;
+        System.out.println(c+d);
+        System.out.println(c-d);
+        System.out.println(c*d);
+        System.out.println(c/d);
+
+
+;
+
+    }
+}
