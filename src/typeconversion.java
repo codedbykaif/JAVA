564c70fd60870c3d921e2826bf7160e12d4543e5
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-22 14:00:24 +0530

    Assignment

--- src/typeconversion.java
+++
@@ -1,9 +0,0 @@
-public class typeconversion {
-    public static void main(String[] args){
-        float myFloat = 4;
-        System.out.println(myFloat);
-
-        int myInt = (int) 64.64f;
-        System.out.println(myInt);
-    }
-}
