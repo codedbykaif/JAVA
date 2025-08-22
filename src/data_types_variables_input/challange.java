f024bf15aed8fbcd8baa914e1d1ec33dba1675a2
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-22 11:29:02 +0530

    user_input

--- src/data_types_variables_input/challange.java
+++ src/data_types_variables_input/challange.java
@@ -1,4 +1,11 @@
-package data_types_variables_input;
-
 public class challange {
+    public static void main(String[] args){
+
+//        challenge first
+        System.out.print("*\n**\n***\n****\n*****");
+//    challenge second
+        System.out.print("*****\n****\n***\n**\n*");
+//        Third challenge
+        System.out.println("    *\n    **\n   ***\n  ****\n *****");
+    }
 }
---
+++ src/data_types_variables_input/user_input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class user_input{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Good morning\n" + name);
+    }
+}
