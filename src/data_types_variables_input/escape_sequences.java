5f087c81ef5dd5ab37f96ac4ce5d58d560f33d0f
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-22 11:04:33 +0530

    escape sequences

---
+++ src/data_types_variables_input/escape_sequences.java
@@ -0,0 +1,12 @@
+public class escape_sequences {
+    public static void main(String[] args){
+
+        System.out.println("Hello 'kaif' dreamer");
+        System.out.println("Hello \"kaif\" dreamer");
+        System.out.println("Hello \\kaif\\ dreamer");
+        System.out.println("Hello \nkaif dreamer");
+        System.out.println("Hello \tkaif dreamer");
+        System.out.println("Hello \bkaif dreamer");
+    }
+    }
+
