d895f57a5e44669f3392997c0e0a70717517913b
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-24 12:36:12 +0530

    learningIF

---
+++ src/Operators_ifElse_numberSystem/learningIF.java
@@ -0,0 +1,16 @@
+
+
+public class learningIF {
+    public static void main(String[] args) {
+         boolean isMale = false;
+         String name = "Bob";
+        System.out.println("Before if");
+         if(isMale){
+             System.out.println("Mr." + name);
+         }
+         else {
+             System.out.println("ms." + name);
+         }
+        System.out.println("After if");
+    }
+}
