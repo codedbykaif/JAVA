725e5a84ddb30784705fd4a63118ca32225208f8
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-24 12:50:05 +0530

    learningIF

--- src/Operators_ifElse_numberSystem/learningIF.java
+++ src/Operators_ifElse_numberSystem/learningIF.java
@@ -2,15 +2,28 @@
 
 public class learningIF {
     public static void main(String[] args) {
-         boolean isMale = false;
-         String name = "Bob";
+        boolean isMale = false;
+        String name = "Bob";
         System.out.println("Before if");
-         if(isMale){
-             System.out.println("Mr." + name);
-         }
-         else {
-             System.out.println("ms." + name);
-         }
+        if (isMale) {
+            System.out.println("Mr." + name);
+        } else {
+            System.out.println("ms." + name);
+        }
         System.out.println("After if");
+
+        boolean isSeniorCitizen = false;
+        boolean isAnAdult = false;
+
+
+        if(isSeniorCitizen) {
+            System.out.println("Hello senior citizen");
+        }
+        else if (isAnAdult) {
+                System.out.println("Hello adult");
+            } else {
+                System.out.println("Hello child");
+            }
+        }
     }
-}
+
