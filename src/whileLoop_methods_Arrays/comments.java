67fd38b5b6e9429a9f62ea5fd074d8dfc1b564e2
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-25 23:50:33 +0530

    comments

---
+++ src/whileLoop_methods_Arrays/comments.java
@@ -0,0 +1,12 @@
+package whileLoop_methods_Arrays;
+
+public class comments {
+    public static void main(String[] args) {
+//        this is important don not delete this
+        System.out.println("Hello world");   //this is single line comment
+        /*
+        this is multi line comment
+        we can add comments in more than one line using this method
+         */
+    }
+}
