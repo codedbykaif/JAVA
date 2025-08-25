965fb80441db88ef0c432b0c64d65c7434e2b407
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-26 00:24:00 +0530

    loop

---
+++ src/whileLoop_methods_Arrays/loop.java
@@ -0,0 +1,16 @@
+package whileLoop_methods_Arrays;
+
+public class loop {
+    public static void main(String[] args) {
+//        int num = 1;                                // initialization
+//        while( num <=10000){                             // condition
+//            System.out.println("\t" +num);          // actual work
+//            num = num + 1;                       //updating the condition
+//        }
+        int count = 500 ;
+        while(count >= 200){
+            System.out.println(count);
+            count = count - 1 ;
+        }
+    }
+}
