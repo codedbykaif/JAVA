52e496e9be4a6c81b42adf7c189dfe89a4732537
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-25 22:30:40 +0530

    bitwiseOR

---
+++ src/Challenges/bitwiseOr.java
@@ -0,0 +1,16 @@
+package Challenges;
+import java.util.Scanner;
+public class bitwiseOr
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Showcasing bitwise Or operator");
+        System.out.print("Enter first number: ");
+        int first = input.nextInt();
+        System.out.print("Enter second number: " );
+        int second = input.nextInt();
+        int result = first & second;
+        System.out.println("Result is: " + result);
+
+    }
+}
