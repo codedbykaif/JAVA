fa151d7d9a4d9bf0683ce2c9919caf99c7d04596
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-26 00:30:06 +0530

    Loop

--- src/whileLoop_methods_Arrays/loop.java
+++ src/whileLoop_methods_Arrays/loop.java
@@ -1,5 +1,5 @@
 package whileLoop_methods_Arrays;
-
+import java.util.Scanner;
 public class loop {
     public static void main(String[] args) {
 //        int num = 1;                                // initialization
@@ -7,10 +7,18 @@ public class loop {
 //            System.out.println("\t" +num);          // actual work
 //            num = num + 1;                       //updating the condition
 //        }
-        int count = 500 ;
-        while(count >= 200){
-            System.out.println(count);
-            count = count - 1 ;
+//        int count = 500 ;
+//        while(count >= 200){
+//            System.out.println(count);
+//            count = count - 1 ;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int i  = 0;
+            while(i < 5){
+                int inp = input.nextInt();
+                System.out.println("the number is: " + inp);
+                i = i + 1;
+            }
         }
     }
-}
+
