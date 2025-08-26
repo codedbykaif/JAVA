27449476ce86951823ad2738a7445a0a76acb8f7
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-26 18:33:23 +0530

    functions

---
+++ src/whileLoop_methods_Arrays/function.java
@@ -0,0 +1,14 @@
+package whileLoop_methods_Arrays;
+
+public class function {
+    public static void main(String[] args) {
+        System.out.println("In main method");
+        greetUser();
+        System.out.println("Method calling complete");
+        greetUser();
+    }
+    public static void greetUser() {
+        System.out.println("Good morining from dreamer: ");
+        System.out.println("my favrouite writter is dostoevsky");
+    }
+}
