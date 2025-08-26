b0d18c6a18e8493b2669d2d65f6f5cefaed21610
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-27 00:10:48 +0530

    parameter

---
+++ src/whileLoop_methods_Arrays/parameter.java
@@ -0,0 +1,15 @@
+package whileLoop_methods_Arrays;
+
+public class parameter {
+    public static void main(String[] args) {
+        int num = sumTwoNumbers(53, 533);
+        System.out.println(num);
+
+    }
+    public static int sumTwoNumbers(int first, int second){
+        System.out.println("First number received:  " + first);
+        System.out.println("Second number received: " + second);
+        int sum = first + second;
+        return sum;
+    }
+}
