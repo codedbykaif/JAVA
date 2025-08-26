77948440f995cb7142f2a93dd2882ea83ec32a01
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-26 18:47:08 +0530

    functionsMethod

--- src/whileLoop_methods_Arrays/function.java
+++ src/whileLoop_methods_Arrays/function.java
@@ -6,6 +6,33 @@ public class function {
         greetUser();
         System.out.println("Method calling complete");
         greetUser();
+        printFirstPattern();
+        System.out.println("\n");
+        printSecondNumber();
+        System.out.println("\n");
+        printThirdPatter();
+
+    }
+    public static void printFirstPattern () {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+    }
+    public static void printSecondNumber() {
+        System.out.println("* * * * *");
+        System.out.println("* * * * ");
+        System.out.println("* * * ");
+        System.out.println("* * ");
+        System.out.println("* ");
+    }
+    public static void printThirdPatter(){
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
     }
     public static void greetUser() {
         System.out.println("Good morining from dreamer: ");
