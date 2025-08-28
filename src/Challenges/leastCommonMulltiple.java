2072906dd12895a3f7570f6d66ecd889cdda11c3
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-28 10:26:15 +0530

    leastCommonMultiple

--- src/Challenges/leastCommonMulltiple.java
+++ src/Challenges/leastCommonMulltiple.java
@@ -8,8 +8,21 @@ public class leastCommonMulltiple {
         int num1 = input.nextInt();
         System.out.println("Enter second numner: ");
         int num2 = input.nextInt();
+        int leastCommonFactor = leastCommonMultiple( num1, num2);
+        System.out.println("LCM of two numbers is: " +leastCommonFactor);
 
 
+    }
+    public static int leastCommonMultiple(int num1, int num2){
+        int i =1;
+        while(true){
+            int factor = num1 * i;
+            if(factor % num2 == 0){
+                return factor;
+            }
+
+            i++;
+        }
 
     }
 }
