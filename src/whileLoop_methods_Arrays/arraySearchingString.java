99a623d4f7bd31f8494f9d4c9aa97976838c7cf6
Codedbykaif <kaif_246048@saitm.ac.in>
2025-08-29 19:54:06 +0530

    twoDimensionalArray

---
+++ src/whileLoop_methods_Arrays/arraySearchingString.java
@@ -0,0 +1,12 @@
+package whileLoop_methods_Arrays;
+import java.util.Scanner;
+public class arraySearchingString {
+    public static void main(String[] args) {
+        String[] strArr = new String[3];
+        strArr[0] = "My String";
+
+
+        String[] newStrArr ={"first", "second", "third", "fourth", "fifth"};
+        System.out.println(newStrArr.length);
+    }
+}
