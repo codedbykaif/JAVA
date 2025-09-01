d07f37011a4493cab49ad2a59bd66e4c23034a8d
Codedbykaif <kaif_246048@saitm.ac.in>
2025-09-01 22:22:37 +0530

    SortedArray

---
+++ src/Challenges/checkSortedArray.java
@@ -0,0 +1,38 @@
+package Challenges;
+import java.util.Scanner;
+public class checkSortedArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("this program checks whether the given array is sorted: ");
+        int[] numArr = arrayUtility.inputArray();
+        boolean isInc = isIncreasing(numArr);
+        boolean isDec = isDecreasing(numArr);
+        if(isInc || isDec){
+            System.out.println("Your array is sorted");
+        }
+        else{
+            System.out.println("Your array is not sorted");
+        }
+
+    }
+        public static boolean isIncreasing(int[] numArr){
+        int i =1;
+        while(i < numArr.length){
+            if(numArr[i] > numArr[i-1]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+        }
+        public static boolean isDecreasing(int[] numArr){
+        int i = 1;
+        while(i > numArr.length){
+            if(numArr[i] < numArr[i-1]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+        }
+}
