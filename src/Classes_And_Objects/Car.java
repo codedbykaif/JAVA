565e0a35b2bcbb36df08f97f730ccf3f4e7404ba
codedbykaif <mdkailfmailme@example.com>
2025-09-08 11:03:12 +0530

    code

---
+++ src/Classes_And_Objects/Car.java
@@ -0,0 +1,42 @@
+package Classes_And_Objects;
+
+public class Car {
+    // INSTANCE VARIABLES
+    int noOfWheels;
+    String color;
+    float maxSpeed;
+    float currentFuelInLitres;
+    int noOfSheets;
+
+    Car(String color) {
+        noOfWheels = 4;
+        this.color = color;
+        maxSpeed = 150;
+        currentFuelInLitres = 2;
+        noOfSheets = 5;
+    }
+
+    public Car start(){
+        if(currentFuelInLitres <= 0){
+            System.out.println("Car is out of fuel, cannot start");
+        } else if(currentFuelInLitres < 5){
+            System.out.println("Car is in reserved mode, please refuel");
+        } else{
+            System.out.println("Car is driving, bruhhhhh....");
+        }
+        return this;
+    }
+
+    public void drive(){
+        currentFuelInLitres--;
+        System.out.println("Car is started");
+    }
+
+    public void addFuel(float currentFuelInLitres){
+        this.currentFuelInLitres += currentFuelInLitres;
+    }
+
+    public float getCurrentFuelLevel(){
+        return currentFuelInLitres;
+    }
+}
---
+++ src/Classes_And_Objects/Driver.java
@@ -0,0 +1,17 @@
+package Classes_And_Objects;
+
+public class Driver {
+    public static void main(String[] args) {
+//        Car myCar = new Car();
+//        myCar.addFuel(6);
+//        myCar.drive();
+//        myCar.drive();
+//        myCar.drive();
+//        myCar.addFuel(3);
+//        System.out.println(myCar.getCurrentFuelLevel());
+        Car swift = new Car("Red");
+//        swift.addFuel(6);
+       swift.start().drive();
+        System.out.println(swift.color);
+    }
+}
