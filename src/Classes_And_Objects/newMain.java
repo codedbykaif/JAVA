1951dcb62c0477988dec6582cbe1091302cd81fc
codedbykaif <kaif_246048@saitm.ac.in>
2025-09-08 11:17:31 +0530

    Newmain

--- src/Classes_And_Objects/newMain.java
+++ src/Classes_And_Objects/newMain.java
@@ -1,7 +1,8 @@
 package Classes_And_Objects;
 
-public class main {
+public class newMain {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("the kaif is best coder");
     }
 }
