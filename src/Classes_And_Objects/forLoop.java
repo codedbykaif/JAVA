16c71e003802c4832128c17270f8b549d5931497
codedbykaif <kaif_246048@saitm.ac.in>
2025-09-09 10:10:26 +0530

    forLoop

---
+++ src/Classes_And_Objects/forLoop.java
@@ -0,0 +1,18 @@
+package Classes_And_Objects;
+
+public class forLoop {
+    public static void main(String[] args) {
+//        int i = 1;
+//        while(i <= 100){
+//            System.out.print(i + " ");
+//            i++;
+//        }
+
+
+//        }
+        for(int j =1; j<=100; j++) {
+            System.out.println(j + " ");
+        }
+    }
+
+}
