dc1fa56bd3df40f1b3dc8491a05aa133dc6023b8
codedbykaif <kaif_246048@saitm.ac.in>
2025-09-09 17:37:55 +0530

    Recursion

---
+++ src/Classes_And_Objects/Recursion.java
@@ -0,0 +1,22 @@
+package Classes_And_Objects;
+import java.util.Scanner;
+public class Recursion {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Welcome to factorial generator");
+            System.out.print("Enter your number: ");
+            int num = input.nextInt();
+            long fact = factorialIterative(num);
+            System.out.println("Factorial of your number is: " + fact);
+
+
+
+    }
+    public static long factorialIterative(int num){
+            long result = 1;
+            for(int i =1; i <= num; i++){
+                result *= i;
+            }
+            return result;
+    }
+}
