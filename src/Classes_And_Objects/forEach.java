65b2cf16b671dda330a5f3b65141c278e4beb28b
codedbykaif <kaif_246048@saitm.ac.in>
2025-09-09 11:35:38 +0530

    forEachLoopArray

--- src/Classes_And_Objects/forEach.java
+++ src/Classes_And_Objects/forEach.java
@@ -5,8 +5,15 @@ public class forEach {
         String[] array = new String[] {
                 "Aastha", "Kanhiya", "Shubham", "Karan", "kartik"
         };
-        printArray(array);
+//        printArray(array);
+        printArrayForEach(array);
 
+        }
+
+        public static void printArrayForEach(String[] array){
+        for(String name : array) {
+            System.out.println(name);
+        }
         }
         public static void printArray(String[] array){
         for(int i =0; i < array.length; i++){
