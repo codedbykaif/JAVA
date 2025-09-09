c4bc33f31b0c80a469ea16a5e2e7f67f954305c4
codedbykaif <kaif_246048@saitm.ac.in>
2025-09-09 09:31:29 +0530

    doWhileAge

--- src/Classes_And_Objects/doWhile.java
+++ src/Classes_And_Objects/doWhile.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class doWhile {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter your age: ");
-        int age = input.nextInt();
-        while(age < 0 || age > 100){
-            System.out.print("Please emter your correct age: ");
+//        System.out.print("Please enter your age: ");
+//        int age = input.nextInt();
+//        while(age < 0 || age > 100){
+//            System.out.print("Please emter your correct age: ");
+//            age = input.nextInt();
+//        }
+//        System.out.println("Your age is " + age);
+        int age;
+        do {
+            System.out.print("Please enter your age: ");
             age = input.nextInt();
-        }
-        System.out.println("Your age is " + age);
+        } while( age < 0 || age > 100);
+        System.out.println("Your age is: " + age);
 
     }
 }
