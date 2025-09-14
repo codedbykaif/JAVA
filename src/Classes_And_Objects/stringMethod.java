0198abad4de4a1f271c6fbca125f89c0b2f29d70
codedbykaif <kaif_246048@saitm.ac.in>
2025-09-14 13:38:00 +0530

    stringMethod

---
+++ src/Classes_And_Objects/stringMethod.java
@@ -0,0 +1,12 @@
+package Classes_And_Objects;
+
+public class stringMethod {
+    static void main(String[] args) {
+        String name = "Kaif";
+        int marks = 45;
+        System.out.println("Hello " + name + ", your marks are: " + 45);
+        System.out.printf("Hello %S, your marks are: %d\n", name, marks);
+        System.out.printf("Hello %15S, your marks are: %d", name, marks);
+
+    }
+}
