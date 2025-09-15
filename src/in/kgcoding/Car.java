7dd401c34b3c45b45f24c8c936e3c43bfeed60d0
codedbykaif <kaif_246048@saitm.ac.in>
2025-09-15 21:18:24 +0530

    AccessTest

---
+++ src/in/kgcoding/Car.java
@@ -0,0 +1,33 @@
+package in.kgcoding;
+
+public class Car {
+    public String car;
+    public String color;
+    public String model;
+    public double fuelLevel;
+    private long costOfPurchase;
+
+    public Car(String black, String bmw, int i, int i1){
+
+    }
+
+
+    public Car() {
+        this.car = car;
+        this.color = color;
+        this.model = model;
+        this.fuelLevel = fuelLevel;
+        this.costOfPurchase = costOfPurchase;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "car='" + car + '\'' +
+                ", color='" + color + '\'' +
+                ", model='" + model + '\'' +
+                ", fuelLevel=" + fuelLevel +
+                ", costOfPurchase=" + costOfPurchase +
+                '}';
+    }
+}
