19a2543b086308c355880dff3a72c639e11c292c
codedbykaif <kaif_246048@saitm.ac.in>
2025-09-18 15:49:40 +0530

    patternUsingSinglePrintStatement

---
+++ src/Revision/patternUsingSinglePrintStatement.java
@@ -0,0 +1,16 @@
+package Revision;
+
+public class patternUsingSinglePrintStatement {
+    static void main(String[] args) {
+        System.out.println("Pattern 1: \n");
+        System.out.println("*\n**\n***\n****\n*****");
+
+        System.out.println("\n");
+        System.out.println("Pattern 2: \n");
+        System.out.println("*****\n****\n***\n**\n*");
+        System.out.println("\n");
+        System.out.println("Pattern 3: \n");
+        System.out.println("    *\n   **\n  ***\n ****\n*****");
+
+    }
+}
