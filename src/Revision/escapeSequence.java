3ebc4e4a2a9cc115a914c291531e7d5e982c5617
codedbykaif <kaif_246048@saitm.ac.in>
2025-09-18 15:42:39 +0530

    escapeSequence

---
+++ src/Revision/escapeSequence.java
@@ -0,0 +1,10 @@
+package Revision;
+
+public class escapeSequence {
+    static void main(String[] args) {
+        System.out.println("This is to check the escape sequence\b");
+        System.out.println("kaif\b is the best code learner");
+        System.out.println("kaif\b is the best code\' learner");
+
+    }
+}
