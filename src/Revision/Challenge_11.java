55e37ead4712e28aa67893165bef168d16bb90c6
codedbykaif <kaif_246048@saitm.ac.in>
2025-09-18 20:30:19 +0530

    Challenge_11

---
+++ src/Revision/Challenge_11.java
@@ -0,0 +1,24 @@
+package Revision;
+import java.util.Scanner;
+public class Challenge_11 {
+    static void main(String[] args) {
+        System.out.println("Showcasing a program that calculates the area of triangle: \n");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the base of triangle: ");
+        int base = input.nextInt();
+        System.out.print("Enter the height of triangle: ");
+        int height = input.nextInt();
+        System.out.println("The area of triangle is, " + 0.5*base*height);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
