0bcf4dea22538de065b3c836b3de8632fc4fb734
codedbykaif <kaif_246048@saitm.ac.in>
2025-09-18 16:02:23 +0530

    Challenge_6

---
+++ src/Revision/Challenge6.java
@@ -0,0 +1,18 @@
+package Revision;
+import java.util.Scanner;
+public class Challenge6 {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("A program to add two numbers\n");
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+        System.out.println("the sum of two number is: " + (num1  + num2));
+
+
+
+
+
+    }
+}
