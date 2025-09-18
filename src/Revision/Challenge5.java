552ebf7be59ff357489b718514d6e990dd5981ac
codedbykaif <kaif_246048@saitm.ac.in>
2025-09-18 15:57:50 +0530

    Challenge5

---
+++ src/Revision/Challenge5.java
@@ -0,0 +1,16 @@
+package Revision;
+import java.util.Scanner;
+
+public class Challenge5 {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("Enter your name: ");
+        String name = input.next();
+        System.out.println("Weclome " + name + " to kg coding" );
+
+
+
+
+    }
+}
