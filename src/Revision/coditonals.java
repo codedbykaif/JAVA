801e56c29c3520dc6701f682a7eaa74a1c2a9821
codedbykaif <codedbykaif>
2026-02-23 08:26:32 +0530

    code

---
+++ src/Revision/coditonals.java
@@ -0,0 +1,16 @@
+package Revision;
+import java.util.Scanner;
+
+public class coditonals {
+    static void main(String[] args) {
+
+        int a = 300,b,c;
+        if(a>=400)
+            b = 300;
+            c = 200;
+        System.out.println(b+" "+c);
+
+
+
+    }
+}
